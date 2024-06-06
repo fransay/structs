3a2b45a339d8ac53f8281a821dd556fba0e23993
Francis Osei Annin <francisannin5@gmail.com>
2024-06-06 17:57:34 +0000

    add: add stack

--- stack/py/main.py
+++ stack/py/main.py
@@ -1,19 +1,30 @@
 
 class Stack(object):
-    def __init__(self, array):
+    def __init__(self, array: list):
         self.array = array
 
-    def push(self, element):
+    def push(self, element) -> None:
         """add ann element on top of stack"""
         self.array.append(element)
 
-    def pop(self):
+    def pop(self) -> None:
         """remove element from top of stack"""
         self.array = self.array[:-1]
 
-    def clear(self):
+    def clear(self) -> None:
         """remove all elements from stack"""
         self.array.clear()
+
+    def is_empty(self) -> bool:
+        """check if stack is empty"""
+        return len(self.array) == 0
+
+    def size(self) -> int:
+        return len(self.array)
+
+    def peek(self) -> int:
+        """catch a peek of the first element on the stack"""
+        return self.array[-1]
 
     def __repr__(self):
         """stack object representation"""
